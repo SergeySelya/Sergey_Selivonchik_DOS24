2e347bf11fee3653b335de3b9040a57d83dc0580
Sergey Selivonchik <88445455+SergeySelya@users.noreply.github.com>
2024-10-25 01:39:12 +0300

    hw4: update task3.sh

--- Homework_Lesson4_OS_3/task3.sh
+++ Homework_Lesson4_OS_3/task3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash  
 
-# Check if the right number of arguments are passed  
+# Проверьте, передано ли правильное количество аргументов  
 if [ "$#" -ne 2 ]; then  
     echo "Usage: $0 filename new_extension"  
     exit 1  
@@ -9,20 +9,20 @@ fi
 filename="$1"  
 NEW_EXT="$2"  
 
-# Check if the file exists  
+# Проверка ,существует ли файл 
 if [ ! -e "${filename}" ]; then  
     echo "File does not exist."  
     exit 1  
 fi  
 
-# Extract the current extension  
+# Извлекаем текущее расширение  
 CURRENT_EXT="${filename##*.}"  
 
-# Check if the current extension is the same as the filename (indicating no extension)  
+# Проверка , на наличие расширения в исходном файле
 if [ "$CURRENT_EXT" == "$filename" ]; then  
     echo "The file has no extension."  
 else  
-    # Rename file with new extension  
+    # Меняем расширение файла 
     BASENAME="${filename%.*}"  
     mv "$filename" "$BASENAME.$NEW_EXT"  
     echo "File extension changed to .$NEW_EXT."  
