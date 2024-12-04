66d40a1acb1b3192a0fc6b85912567b4dfc5e7a9
Sergey Selivonchik <elhom99@gmail.com>
2024-12-04 08:48:44 +0300

    add HW12

--- Homework_Lesson11_Bash_Shell_2/log_parser.sh
+++
@@ -1,8 +0,0 @@
-result=$(grep /log_exmp ‘403$’)
-
-for string in result; do
-IFS=' ' read -ra array <<< "$string"
-for element in 
-echo $el | grep -o 'name[^ ]*'
-
-done
---
+++ Homework_Lesson12_Bash_Shell_2/log_parser.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# generate log
+bash ./generate_log.sh
+
+# logs parser
+result=$(grep "403$" ./server.log)
+IFS=$'\n'
+
+for line in $result; do
+    IFS=$' ' read -ra array <<< $line
+    usernames+=(${array[3]})
+    ip_all+=(${array[4]})
+done
+
+# Output Username
+echo -e "\nSuccessful logins (IP addresses):"
+for ip in "${ip_all[@]}"; do  
+    echo "$ip" | cut -c 4-
+done  
+
+# Output Username
+echo -e "\nUsers with failed logins:"
+for name in "${usernames[@]}"; do  
+    echo "$name" | cut -c 6-
+done  
