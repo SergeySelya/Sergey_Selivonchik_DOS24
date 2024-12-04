b929bb8abdc94578e0507869db94ae1940acc333
Sergey Selivonchik <elhom99@gmail.com>
2024-12-04 14:33:48 +0300

    Update HW13

--- Homework_Lesson13_Bash_Shell_3/API_Call_Weather.sh
+++ Homework_Lesson13_Bash_Shell_3/API_Call_Weather.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 api_call_wether() {
-    # bash main_weather.sh (city) (day4,16) (С,F) (lang)
     API_ID="baa12bb7d2ed31c6c4b31e8a5d40d726"
     CITY=$1
     UNITS=$2
     LANG=$3
     COUNT_DAY=$4
-    
 
     # Units
     if [ $UNITS == "i" ]; then
@@ -18,7 +16,6 @@ api_call_wether() {
         echo -e "\nWarn: < ${UNITS} > is not exist you can chose: \n i-imperial \n m-metric\n * metric unit is default \n"
     fi
 
-    # count day
     output=$(curl -X GET "https://api.openweathermap.org/data/2.5/weather?q=$CITY&appid=$API_ID&units=$UNITS&lang=$LANG&cnt=$COUNT_DAY")
     echo $output > weather.json
 }
