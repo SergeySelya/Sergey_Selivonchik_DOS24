abd72ccc958fcd6be9a53cf96f7d71c5b1c0341a
Sergey Selivonchik <88445455+SergeySelya@users.noreply.github.com>
2025-02-26 07:37:38 +0300

    HW28

---
+++ Homework_Lesson28_Jenkins_2/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_access_key" {
+  description = "....."
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "....."
+  type        = string
+  sensitive   = true
+}
